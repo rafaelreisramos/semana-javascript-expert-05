cded3d7a1da857b13e1f45e66883eae08e9c1a2e
rafaelreisramos <7602616+rafaelreisramos@users.noreply.github.com>
2021-09-23 09:31:25 -0300

    refactor: move modal logic to a separated manager (#1)

--- gdrive-app/public/app.js
+++ gdrive-app/public/app.js
@@ -2,6 +2,7 @@ import AppController from './src/appController.js'
 import ConnectionManager from './src/connectionManager.js'
 import DragAndDropManager from './src/dragAndDropManager.js'
 import ViewManager from './src/viewManager.js'
+import ModalManager from './src/modalManager.js'
 
 const API_URL = "https://localhost:3000"
 
@@ -10,7 +11,8 @@ const appController = new AppController({
     apiUrl: API_URL,
   }),
   viewManager: new ViewManager(),
-  dragAndDropManager: new DragAndDropManager()
+  dragAndDropManager: new DragAndDropManager(),
+  modalManager: new ModalManager()
 })
 
 try {
--- gdrive-app/public/src/viewManager.js
+++ gdrive-app/public/src/viewManager.js
@@ -5,11 +5,6 @@ export default class ViewManager {
     this.newFileButton = document.getElementById('newFileButton')
     this.fileElem = document.getElementById('fileElem')
     
-    this.modal = {}
-    this.progressModal = document.getElementById('progressModal')
-    this.progressBar = document.getElementById('progressBar')
-    this.output = document.getElementById('output')
-
     this.formatter = new Intl.DateTimeFormat('pt', {
       locale: 'pt-br',
       month: 'long',
@@ -26,32 +21,6 @@ export default class ViewManager {
 
   configureOnFileChange(fn) {
     this.fileElem.onchange = e => fn(e.target.files)
-  }
-
-  configureModal() {
-    this.modal = M.Modal.init(this.progressModal, {
-      opacity: 0,
-      dismissable: false,
-      
-      onOpenEnd() { // allow click outside modal when open
-        this.$overlay[0].remove()
-      }
-    })
-  }
-
-  openModal() {
-    this.modal.open()
-  }
-
-  closeModal() {
-    this.modal.close()
-  }
-
-  updateProgressBarModalStatus(size) {
-    this.output.innerHTML = `
-      Uploading <b>${Math.floor(size)}%</b>
-    `
-    this.progressBar.value = size
   }
 
   getFileType(file) {
